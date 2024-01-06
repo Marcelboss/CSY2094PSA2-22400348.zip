7dcd6940eaee343238fea7a303c14923e2e52656
Stephen.chibuoke <97477278+Marcelboss@users.noreply.github.com>
2024-01-06 19:56:30 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,24 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieselect = document.getElementById('movie');
 
-let ticketprice = +movieselect.ariaValueMax;
+let ticketprice = +movieselect.Value;
+
+//update total and count 
+function updateSelectedcount() {
+    const Selectedseats = document.querySelectorAll('.row.seat.selected');
+
+    const Selectedseatscount = Selectedseats.length;
+
+
+    count.innerText = Selectedseatscount;
+    total.innerText = Selectedseatscount = ticketprice;
+}
 container.addEventListener('click', (e) => {
-    if (e.target.classlist.contains('seat') && !e.target.classlist.contains('occupied')) {
-        e.target.classlist.toggle('selected');
+    if (e.target.classlist.contains('seat') && !e.target.classlist.contains('occupied')) {;
+      e.target.classlist.toggle('Selected');
     }
+
+    updateSelectedcount();
 });
 
 
